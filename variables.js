a45b94e5eb6644c18abdfba7785476aa3186efd2
Adhyan Gupta <adhyan@Adhyans-Mac.local>
2022-02-16 14:21:51 +0530

    datatypes, comments, variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+// number string floating point number
+// bool undefined null
+
+var isRunning= null;
+console.log(isRunning);
+console.log(typeof(isRunning));
+
+/*
+Multipe line 
+comments
+can be added
+like this
+*/
